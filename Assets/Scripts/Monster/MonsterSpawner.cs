73f97af392b9abfdb413659ff1185f1e84828053
태범 <39542671+xoqja99@users.noreply.github.com>
2022-08-13 21:20:53 +0900

    사위 몬스터 프리팹 제작, 몬스터 스탯 테이블화, 공격패턴 코루틴 함수로 수정

--- Assets/Scripts/Monster/MonsterSpawner.cs
+++ Assets/Scripts/Monster/MonsterSpawner.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 public class MonsterSpawner : MonoBehaviour
 {
-    private Monster monsterPrefab; // 생성할 몬스터 Prefab
+    public Monster monsterPrefab; // 생성할 몬스터 Prefab
 
     public List<Vector3> spawnPoints; // 스폰 위치
+    public Transform[] spawnPointsTest; // !! 테스트 코드
 
     private List<Monster> monsters = new List<Monster>(); // 생성된 몬스터들을 담는 리스트
 
-    //private void OnEnable()
-    //{
-    //    // 몬스터 스폰
-    //    CreateEnemy();
-    //}
+    private void OnEnable()
+    {
+        CreateEnemyTest(); // !! 테스트 코드
+    }
 
     // 몬스터를 생성하고 추적할 대상 할당
     private void CreateEnemy()
@@ -32,6 +32,20 @@ public class MonsterSpawner : MonoBehaviour
         }
     }
 
+    // !! 테스트 코드
+    private void CreateEnemyTest()
+    {
+        foreach (Transform spawnPoint in spawnPointsTest)
+        {
+            Monster monster = Instantiate(monsterPrefab, spawnPoint.position, spawnPoint.rotation);
+
+            monsters.Add(monster);
+            monster.onDeath += () => monsters.Remove(monster);
+            monster.onEliminate += () => Destroy(monster.gameObject);
+            monster.onRevive += () => monsters.Add(monster);
+        }
+    }
+
     public void Set(List<Vector3> spawnPositions)
     {
         spawnPoints = spawnPositions;
@@ -40,6 +54,6 @@ public class MonsterSpawner : MonoBehaviour
 
     public void Spawn()
     {
-
+        
     }
 }
--- Assets/Scripts/Monster/RushZombie.cs
+++ Assets/Scripts/Monster/RushZombie.cs
@@ -4,62 +4,40 @@ using UnityEngine;
 public class RushZombie : Monster
 {
     bool rushing = false; // 돌진 중 여부
-    protected float rushCoolTime = 5.5f; // 돌진 쿨타임
-    protected float lastRushTime = -4.5f; // 마지막 돌진 시점
-
-    bool rushReady = false; // 돌진 준비중 여부
+    protected float rushCoolTime = 5f; // 돌진 쿨타임
+    protected float lastRushTime = -4f; // 마지막 돌진 시점
     protected float timeForRushReady = 1f; // 돌진 준비시간
 
-    // 몬스터 초기화
-    protected override void Setup()
+    // 돌진 상태 수행
+    protected virtual IEnumerator Rushing()
     {
-        maxHealth = 125f;
-        corpseHealth = 75f;
-        damage = 20f;
-        speed = 1f;
+        bool rushing = true;
+        bool rushReady = true;
 
-        base.Setup();
-    }
+        while (!dead && rushing)
+        {
+            if (Time.time < lastRushTime + timeForRushReady) // 대기
+            {
+                rigidbody2d.velocity = Vector2.zero;
+            }
+            else if (rushReady == true) // 돌진
+            {
+                rigidbody2d.AddForce(direction * 300f);
+                damage = 40f;
+                rushReady = false;
+                animator.SetTrigger("Attack_Normal");
+            }
+            else if (Time.time >= lastRushTime + timeForRushReady + 0.5f) // 돌진 종료
+            {
+                damage = 20f;
+                rushing = false;
+            }
 
-    protected override void Update()
-    {
-        // 현재 행동 수행
-        if (dead)  // **플레이어 사망여부 추가
-        {
-            rigidbody2d.velocity = Vector2.zero;
-        }
-        else if (action == "moving")
-        {
-            Moving();
-        }
-        else if (action == "rushing")
-        {
-            Rushing();
+            yield return new WaitForSeconds(0.05f);
         }
 
-        // 추적 대상의 존재 여부에 따라 다른 애니메이션을 재생
-        animator.SetBool("HasTarget", hasTarget);
-    }
-
-    // 돌진 수행
-    protected void Rushing()
-    {
-        if (Time.time < lastRushTime + timeForRushReady)
-        {
-            rigidbody2d.velocity = Vector2.zero;
-        }
-        else if (rushReady == true)
-        {
-            rigidbody2d.AddForce(direction * 300f);
-            damage = 40f;
-            rushReady = false;
-        }
-        else if (Time.time >= lastRushTime + timeForRushReady + 0.5f)
-        {
-            action = "moving";
-            damage = 20f;
-            rushing = false;
-        }
+        action = "moving";
+        StartCoroutine(Moving());
     }
 
     private void OnTriggerStay2D(Collider2D other)
@@ -75,10 +53,9 @@ public class RushZombie : Monster
             Entity entity = other.gameObject.GetComponent<Entity>();
             if (targetEntity == entity)
             {
-                action = "rushing";
                 lastRushTime = Time.time;
-                rushing = true;
-                rushReady = true;
+                action = "rushing";
+                StartCoroutine(Rushing());
             }
         }
     }
